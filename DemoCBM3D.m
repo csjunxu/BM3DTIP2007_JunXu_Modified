b6ea4cc88ed31a85cf46e3609eeb423d671bbfee
XuJun <nankaimathxujun@gmail.com>
2017-01-25 00:31:13 +0800

    add

--- DemoCBM3D.m
+++ DemoCBM3D.m
@@ -1,11 +1,24 @@
 clear;
-GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_ccnoise_denoised_part\';
-GT_fpath = fullfile(GT_Original_image_dir, '*mean.png');
-TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_ccnoise_denoised_part\';
-TT_fpath = fullfile(TT_Original_image_dir, '*real.png');
+
+%% read  image directory
+GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\DJI_Results\Real_MeanImage\';
+GT_fpath = fullfile(GT_Original_image_dir, '*.JPG');
+TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\DJI_Results\Real_NoisyImage\';
+TT_fpath = fullfile(TT_Original_image_dir, '*.JPG');
+% GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_ccnoise_denoised_part\';
+% GT_fpath = fullfile(GT_Original_image_dir, '*mean.png');
+% TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_ccnoise_denoised_part\';
+% TT_fpath = fullfile(TT_Original_image_dir, '*real.png');
 GT_im_dir  = dir(GT_fpath);
 TT_im_dir  = dir(TT_fpath);
 im_num = length(TT_im_dir);
+
+method = 'CBM3D';
+%% write image directory
+write_sRGB_dir = ['C:/Users/csjunxu/Desktop/CVPR2017/DJI_Results/' method '/'];
+if ~isdir(write_sRGB_dir)
+    mkdir(write_sRGB_dir)
+end
 
 colorspace = 'opp' ;
 print_to_screen = 0;
@@ -24,20 +37,19 @@ for i = 1 : im_num
     CCPSNR = [CCPSNR csnr( IMin*255,IM_GT*255, 0, 0 )];
     CCSSIM = [CCSSIM cal_ssim( IMin*255, IM_GT*255, 0, 0 )];
     fprintf('The initial PSNR = %2.4f, SSIM = %2.4f. \n', CCPSNR(end), CCSSIM(end));
-    IMout = zeros(size(IMin));
     %% denoising
-%     nSig = NoiseLevel( IMin*255);
-    nSig = NoiseEstimation( IMin*255,8); 
+    %     nSig = NoiseLevel( IMin*255);
+    nSig = NoiseEstimation( IMin*255, 8);
     [~, IMout] = CBM3D(IM_GT, IMin, nSig, profile, print_to_screen, colorspace);
     %% output
     PSNR = [PSNR csnr( IMout*255, IM_GT*255, 0, 0 )];
     SSIM = [SSIM cal_ssim( IMout*255, IM_GT*255, 0, 0 )];
     fprintf('The final PSNR = %2.4f, SSIM = %2.4f. \n', PSNR(end), SSIM(end));
     %% output
-    imwrite(IMout, ['C:/Users/csjunxu/Desktop/CVPR2017/cc_Results/Real_CBM3D/CBM3D_BID_' IMname '.png']);
+    imwrite(IMout, [write_sRGB_dir method '_RID_' IMname '.png']);
 end
 mPSNR = mean(PSNR);
 mSSIM = mean(SSIM);
 mCCPSNR = mean(CCPSNR);
 mCCSSIM = mean(CCSSIM);
-save(['C:/Users/csjunxu/Desktop/CVPR2017/cc_Results/Real_CBM3D_BID_CCNoise_15.mat'],'PSNR','mPSNR','SSIM','mSSIM','CCPSNR','mCCPSNR','CCSSIM','mCCSSIM');
+save(['C:/Users/csjunxu/Desktop/CVPR2017/DJI_Results/CBM3D_RID.mat'],'PSNR','mPSNR','SSIM','mSSIM','CCPSNR','mCCPSNR','CCSSIM','mCCSSIM');
