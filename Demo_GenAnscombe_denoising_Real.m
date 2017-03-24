79a0beb405455f84f8ab5c04e6ccedcc01a8e74f
XuJun <nankaimathxujun@gmail.com>
2017-03-24 11:56:05 +0800

    add

--- Demo_GenAnscombe_denoising_Real.m
+++ Demo_GenAnscombe_denoising_Real.m
@@ -27,7 +27,7 @@ for alpha = [1]
     
     PSNR_yhat_alg   =  [];
     SSIM_yhat_alg   =  [];
-    for i = 19:im_num
+    for i = 21:im_num
         
         z = im2double( imread(fullfile(TT_Original_image_dir, TT_im_dir(i).name)) );
         y = im2double( imread(fullfile(GT_Original_image_dir, GT_im_dir(i).name)) );
@@ -37,7 +37,7 @@ for alpha = [1]
         [h, w, ch] = size(z);
         
         % mixed Poisson-Gaussian noise parameters
-        
+        yhat_alg  = zeros(size(z));
         % Poisson scaling factor
         for c = 1:ch
             % Gaussian component N(g,sigma^2)
@@ -45,7 +45,7 @@ for alpha = [1]
             g = 0.0;
             
             %% Apply forward variance stabilizing transformation
-            fz = GenAnscombe_forward(z(:, :, c), sigma,alpha,g); % Generalized Anscombe VST (J.L. Starck, F. Murtagh, and A. Bijaoui, Image  Processing  and  Data Analysis, Cambridge University Press, Cambridge, 1998)
+            fz = GenAnscombe_forward(z(:, :, c), sigma, alpha, g); % Generalized Anscombe VST (J.L. Starck, F. Murtagh, and A. Bijaoui, Image  Processing  and  Data Analysis, Cambridge University Press, Cambridge, 1998)
             
             %% DENOISING
             
@@ -67,22 +67,22 @@ for alpha = [1]
             D = D*(maxzans-minzans)+minzans;
             
             %% Apply the inverse transformation
-%             yhat(:, :, c) = GenAnscombe_inverse_exact_unbiased(D,sigma,alpha,g);   % exact unbiased inverse
-%             yhat_cfa(:, :, c) = GenAnscombe_inverse_closed_form(D,sigma,alpha,g);  % closed-form approximation
-%             yhat_asy(:, :, c) =  (D/2).^2 - 1/8 - sigma^2;                       % asymptotical inverse
+            %             yhat(:, :, c) = GenAnscombe_inverse_exact_unbiased(D,sigma,alpha,g);   % exact unbiased inverse
+            %             yhat_cfa(:, :, c) = GenAnscombe_inverse_closed_form(D,sigma,alpha,g);  % closed-form approximation
+            %             yhat_asy(:, :, c) =  (D/2).^2 - 1/8 - sigma^2;                       % asymptotical inverse
             yhat_alg(:, :, c) =  (D/2).^2 - 3/8 - sigma^2;                       % algebraic inverse
         end
         
         
-%         PSNR_yhat   =  [PSNR_yhat csnr( y*255, yhat*255, 0, 0 )];
-%         SSIM_yhat   = [SSIM_yhat  cal_ssim( y*255, yhat*255, 0, 0 )];
-%         fprintf('%s : PSNR = %2.4f, SSIM = %2.4f \n', TT_im_dir(i).name, PSNR_yhat(end), SSIM_yhat(end );
-%         PSNR_yhat_cfa   = [PSNR_yhat_cfa  csnr( y*255, yhat_cfa*255, 0, 0 )];
-%         SSIM_yhat_cfa   = [SSIM_yhat_cfa  cal_ssim( y*255, yhat_cfa*255, 0, 0 )];
-%         fprintf('%s : PSNR = %2.4f, SSIM = %2.4f \n', TT_im_dir(i).name, PSNR_yhat_cfa(end), SSIM_yhat_cfa(end) );
-%         PSNR_yhat_asy   = [PSNR_yhat_asy  csnr( y*255, yhat_asy*255, 0, 0 )];
-%         SSIM_yhat_asy   =  [SSIM_yhat_asy cal_ssim( y*255, yhat_asy*255, 0, 0 )];
-%         fprintf('%s : PSNR = %2.4f, SSIM = %2.4f \n', TT_im_dir(i).name, PSNR_yhat_asy(end), SSIM_yhat_asy(end) );
+        %         PSNR_yhat   =  [PSNR_yhat csnr( y*255, yhat*255, 0, 0 )];
+        %         SSIM_yhat   = [SSIM_yhat  cal_ssim( y*255, yhat*255, 0, 0 )];
+        %         fprintf('%s : PSNR = %2.4f, SSIM = %2.4f \n', TT_im_dir(i).name, PSNR_yhat(end), SSIM_yhat(end );
+        %         PSNR_yhat_cfa   = [PSNR_yhat_cfa  csnr( y*255, yhat_cfa*255, 0, 0 )];
+        %         SSIM_yhat_cfa   = [SSIM_yhat_cfa  cal_ssim( y*255, yhat_cfa*255, 0, 0 )];
+        %         fprintf('%s : PSNR = %2.4f, SSIM = %2.4f \n', TT_im_dir(i).name, PSNR_yhat_cfa(end), SSIM_yhat_cfa(end) );
+        %         PSNR_yhat_asy   = [PSNR_yhat_asy  csnr( y*255, yhat_asy*255, 0, 0 )];
+        %         SSIM_yhat_asy   =  [SSIM_yhat_asy cal_ssim( y*255, yhat_asy*255, 0, 0 )];
+        %         fprintf('%s : PSNR = %2.4f, SSIM = %2.4f \n', TT_im_dir(i).name, PSNR_yhat_asy(end), SSIM_yhat_asy(end) );
         PSNR_yhat_alg   = [PSNR_yhat_alg  csnr( y*255, yhat_alg*255, 0, 0 )];
         SSIM_yhat_alg   = [SSIM_yhat_alg  cal_ssim( y*255, yhat_alg*255, 0, 0 )];
         fprintf('%s : PSNR = %2.4f, SSIM = %2.4f \n', TT_im_dir(i).name, PSNR_yhat_alg(end), SSIM_yhat_alg(end) );
