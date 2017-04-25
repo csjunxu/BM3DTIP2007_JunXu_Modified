13d2e5f2828804e62bac597501d18ab5689abb2b
XuJun <nankaimathxujun@gmail.com>
2017-04-25 13:20:46 +0800

    ad

--- BM3D_GaussianNoise.m
+++ BM3D_GaussianNoise.m
@@ -1,38 +1,32 @@
 clear;
 
-Original_image_dir  =    'C:\Users\csjunxu\Desktop\ECCV2016\grayimages\';
+Original_image_dir  =    'C:\Users\csjunxu\Desktop\Projects\WODL\20images\';
+% Original_image_dir  =    'C:\Users\csjunxu\Desktop\Projects\WODL\20newimages\';
+Sdir = regexp(Original_image_dir, '\', 'split');
 fpath = fullfile(Original_image_dir,'*.png');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
-for nSig = [20 30 40 50 75]
-    for Sample = 1:1
-        imPSNR{Sample} = [];
-        imSSIM{Sample} = [];
-        for i = 1:im_num
-            S = regexp(im_dir(i).name, '\.', 'split');
-            %                     read the clean image and normalization
-            I = double(imread( strcat(Original_image_dir,im_dir(i).name) ))/255;
-            randn('seed',Sample-1);
-            noiseI = I + nSig/255*randn(size(I));
-            EnSig = NoiseLevel(noiseI*255);
-            fprintf(' The estimated noise level is :%2.2f. \n',EnSig);
-            %                     Denoise 'noiseI'. The denoised image is 'restoredI'
-            [~, restoredI] = BM3D(I, noiseI, EnSig);
-            %                     Compute the putput PSNR
-            imPSNR{Sample} = [imPSNR{Sample} csnr( restoredI*255, I*255, 0, 0 )];
-            imSSIM{Sample}  = [imSSIM{Sample} cal_ssim( restoredI*255, I*255, 0, 0 )];
-            restoredIn = sprintf('C:/Users/csjunxu/Desktop/ECCV2016/1_Results/BM3D/Gaussian/BM3D_nSig%d_EnSig%d_Sample%d_%s',nSig,EnSig,Sample,im_dir(i).name);
-            imwrite(restoredI,restoredIn,'png');
-            fprintf('PSNR is:%f, SSIM is %f\n',csnr( restoredI*255, I*255, 0, 0 ),cal_ssim( restoredI*255, I*255, 0, 0 ));
-        end
-        fprintf('The %dth Sampling is over. \n',Sample);
-        SmPSNR(Sample) = mean(imPSNR{Sample});
-        SmSSIM(Sample) = mean(imSSIM{Sample});
-        Smatname = sprintf('BM3D_nSig%d.mat',nSig);
-        save(Smatname,'nSig','imPSNR','imSSIM','SmPSNR','SmSSIM');
+for nSig = [20 40 60 80 100]
+    PSNR = [];
+    SSIM = [];
+    for i = 1:im_num
+        S = regexp(im_dir(i).name, '\.', 'split');
+        %                     read the clean image and normalization
+        I = double(imread( strcat(Original_image_dir,im_dir(i).name) ))/255;
+        randn('seed', 0);
+        noiseI = I + nSig/255*randn(size(I));
+        fprintf(' The noise level is :%2.2f. \n',nSig);
+        %                     Denoise 'noiseI'. The denoised image is 'restoredI'
+        [~, restoredI] = BM3D(I, noiseI, nSig);
+        %                     Compute the putput PSNR
+        PSNR = [PSNR csnr( restoredI*255, I*255, 0, 0 )];
+        SSIM = [SSIM cal_ssim( restoredI*255, I*255, 0, 0 )];
+        restoredIn = sprintf('C:/Users/csjunxu/Desktop/NIPS2017/W3Results/BM3D_nSig%d_%s',nSig,im_dir(i).name);
+        imwrite(restoredI,restoredIn,'png');
+        fprintf('PSNR is:%f, SSIM is %f\n',csnr( restoredI*255, I*255, 0, 0 ),cal_ssim( restoredI*255, I*255, 0, 0 ));
     end
-    mPSNR = mean(SmPSNR);
-    mSSIM = mean(SmSSIM);
-    matname = sprintf('C:/Users/csjunxu/Desktop/ECCV2016/1_Results/BM3D/BM3D_nSig%d_Sample%d.mat',nSig,Sample);
-    save(matname,'nSig','SmPSNR','SmSSIM','mPSNR','mSSIM');
+    mPSNR = mean(PSNR);
+    mSSIM = mean(SSIM);
+    matname = sprintf('C:/Users/csjunxu/Documents/GitHub/WODL_RID/Gaussian/BM3D_nSig%d.mat',nSig);
+    save(matname,'nSig','PSNR','SSIM','mPSNR','mSSIM');
 end
