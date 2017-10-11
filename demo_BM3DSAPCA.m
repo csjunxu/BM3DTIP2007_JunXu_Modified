f9cc562351afa9e1ad3d2415ac449c17f71b8bc9
XuJun <nankaimathxujun@gmail.com>
2017-10-11 12:44:59 +0800

    A

---
+++ demo_BM3DSAPCA.m
@@ -0,0 +1,67 @@
+% BM3D-SAPCA : BM3D with Shape-Adaptive Principal Component Analysis  (v1.00, 2009)
+% (demo script)
+%
+% BM3D-SAPCA is an algorithm for attenuation of additive white Gaussian noise (AWGN)
+% from grayscale images. This algorithm reproduces the results from the article:
+%  K. Dabov, A. Foi, V. Katkovnik, and K. Egiazarian, "BM3D Image Denoising with
+%  Shape-Adaptive Principal Component Analysis", Proc. Workshop on Signal Processing
+%  with Adaptive Sparse Structured Representations (SPARS'09), Saint-Malo, France,
+%  April 2009.     (PDF available at  http://www.cs.tut.fi/~foi/GCF-BM3D )
+%
+%
+% SYNTAX:
+%
+%     y_est = BM3DSAPCA2009(z, sigma)
+%
+% where  z  is an image corrupted by AWGN with noise standard deviation  sigma
+% and  y_est  is an estimate of the noise-free image.
+% Signals are assumed on the intensity range [0,1].
+%
+%
+% USAGE EXAMPLE:
+%
+%     y = im2double(imread('Cameraman256.png'));
+%     sigma=25/255;
+%     z=y+sigma*randn(size(y));
+%     y_est = BM3DSAPCA2009(z,sigma);
+%
+%
+%
+% Copyright (c) 2009-2011 Tampere University of Technology.   All rights reserved.
+% This work should only be used for nonprofit purposes.
+%
+% author:  Alessandro Foi,   email:  firstname.lastname@tut.fi
+%
+%%
+
+clear all
+addpath('BM3D-SAPCA');
+Original_image_dir  =    'C:\Users\csjunxu\Desktop\Projects\WODL\20images\';
+Sdir = regexp(Original_image_dir, '\', 'split');
+fpath = fullfile(Original_image_dir,'*.png');
+im_dir  = dir(fpath);
+im_num = length(im_dir);
+for nSig = [20 40 60 80 100]
+    PSNR = [];
+    SSIM = [];
+    for i = 1:im_num
+        S = regexp(im_dir(i).name, '\.', 'split');
+        %                     read the clean image and normalization
+        I = double(imread( strcat(Original_image_dir,im_dir(i).name) ))/255;
+        randn('seed', 0);
+        noiseI = I + nSig/255*randn(size(I));
+        fprintf(' The noise level is :%2.2f. \n',nSig);
+        %                     Denoise 'noiseI'. The denoised image is 'restoredI'
+        restoredI = BM3DSAPCA2009(noiseI,nSig);
+        %                     Compute the putput PSNR
+        PSNR = [PSNR csnr( restoredI*255, I*255, 0, 0 )];
+        SSIM = [SSIM cal_ssim( restoredI*255, I*255, 0, 0 )];
+        restoredIn = sprintf('C:/Users/csjunxu/Desktop/NIPS2017/W3Results/BM3DSAPCA_nSig%d_%s',nSig,im_dir(i).name);
+        imwrite(restoredI,restoredIn,'png');
+        fprintf('PSNR is:%f, SSIM is %f\n',csnr( restoredI*255, I*255, 0, 0 ),cal_ssim( restoredI*255, I*255, 0, 0 ));
+    end
+    mPSNR = mean(PSNR);
+    mSSIM = mean(SSIM);
+    matname = sprintf('C:/Users/csjunxu/Documents/GitHub/WODL_RID/Gaussian/BM3DSAPCA_nSig%d.mat',nSig);
+    save(matname,'nSig','PSNR','SSIM','mPSNR','mSSIM');
+end
